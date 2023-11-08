f8b411ef50263e562e6e1c39de6276ecdeca833c
petrelevich <petrelevich@yandex.ru>
2023-11-08 20:36:47 +0300

    L02 gradle, logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 
 plugins {
     idea
+    id("fr.brouillard.oss.gradle.jgitver")
     id("io.spring.dependency-management")
     id("org.springframework.boot") apply false
     id("name.remal.sonarlint") apply false
@@ -44,6 +45,26 @@ allprojects {
             dependency("com.google.guava:guava:$guava")
         }
     }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("javax.servlet:servlet-api:2.4")
+            force("commons-logging:commons-logging:1.1.1")
+            force("commons-lang:commons-lang:2.5")
+            force("org.codehaus.jackson:jackson-core-asl:1.8.8")
+            force("org.codehaus.jackson:jackson-mapper-asl:1.8.3")
+            force("org.codehaus.jettison:jettison:1.1")
+            force("net.java.dev.jna:jna:5.8.0")
+            force("com.google.errorprone:error_prone_annotations:2.7.1")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-commons:2.3.0.1263")
+            force("com.google.code.findbugs:jsr305:3.0.2")
+            force("org.sonarsource.sslr:sslr-core:1.24.0.633")
+            force("org.eclipse.platform:org.eclipse.osgi:3.18.400")
+            force("org.eclipse.platform:org.eclipse.equinox.common:3.18.0")
+        }
+    }
 }
 
 subprojects {
@@ -63,6 +84,26 @@ subprojects {
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         java {
             palantirJavaFormat("2.38.0")
+        }
+    }
+
+    plugins.apply(fr.brouillard.oss.gradle.plugins.JGitverPlugin::class.java)
+    extensions.configure<fr.brouillard.oss.gradle.plugins.JGitverPluginExtension> {
+        strategy("PATTERN")
+        nonQualifierBranches("main,master")
+        tagVersionPattern("\${v}\${<meta.DIRTY_TEXT}")
+        versionPattern(
+                "\${v}\${<meta.COMMIT_DISTANCE}\${<meta.GIT_SHA1_8}" +
+                        "\${<meta.QUALIFIED_BRANCH_NAME}\${<meta.DIRTY_TEXT}-SNAPSHOT"
+        )
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
         }
     }
 }
