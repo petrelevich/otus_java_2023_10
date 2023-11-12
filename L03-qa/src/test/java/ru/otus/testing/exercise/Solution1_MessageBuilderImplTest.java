6e44d83a848d102a4792bf298a8e2c9be2d3fc53
stvort <stvort@mail.ru>
2023-11-13 01:37:18 +0400

    L03-qa added

---
+++ L03-qa/src/test/java/ru/otus/testing/exercise/Solution1_MessageBuilderImplTest.java
@@ -0,0 +1,14 @@
+package ru.otus.testing.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"java:S2699"})
+class Solution1_MessageBuilderImplTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void buildMessageTest() {}
+}
