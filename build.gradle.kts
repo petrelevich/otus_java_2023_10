2926395f93b0a9716cb5143d89786887daffa0aa
petrelevich <petrelevich@yandex.ru>
2024-03-28 17:16:00 +0300

    L37 webflux

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,8 @@ allprojects {
     val jsr305: String by project
 	
 	val grpc: String by project
+    val wiremock: String by project
+    val r2dbcPostgresql: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
@@ -101,6 +103,8 @@ allprojects {
 			dependency("io.grpc:grpc-netty:$grpc")
             dependency("io.grpc:grpc-protobuf:$grpc")
             dependency("io.grpc:grpc-stub:$grpc")
+            dependency("com.github.tomakehurst:wiremock-standalone:$wiremock")
+            dependency("io.r2dbc:r2dbc-postgresql:$r2dbcPostgresql")
         }
     }
 
